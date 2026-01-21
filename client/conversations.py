ff52d9dbe674f6ebf97e2d537cd91381f404af25
Gabriel Haberkamp <gabrielhaberkamp@gmail.com>
2026-01-21 15:37:24 -0300

    Multiple Improvements on the Interface

--- client/conversations.py
+++ client/conversations.py
@@ -13,6 +13,7 @@ class ConversationsPanel(wx.Panel):
         self.main_window = main_window
         self.parent = parent
         self.init_UI()
+        self.create_accelerator_table()
 
     def init_UI(self):
         self.conversations_label = wx.StaticText(self, label=self.main_window.i18n.t("conversations"), pos=(10,10))
@@ -24,11 +25,35 @@ class ConversationsPanel(wx.Panel):
         self.message_label = wx.StaticText(self.conversation_panel, label=self.main_window.i18n.t("type_message"))
         self.message_field = wx.TextCtrl(self.conversation_panel, style=wx.TE_MULTILINE | wx.TE_PROCESS_ENTER | wx.TE_DONTWRAP, size=(300, 25))
         self.record_voice_message_btn = CmdBtn(self.conversation_panel, mainLabel=self.main_window.i18n.t("record_voice_message"), note="Ctrl+R", size=(150, 40))
+        self.record_voice_message_btn.Bind(wx.EVT_BUTTON, self.on_record_voice_message)
 
     def on_conversation_selected(self, event):
         self.conversation = self.main_window.chats[event.GetIndex()]
         self.conversation_name = self.main_window.chat_names[event.GetIndex()]
+        print(self.conversation)
         #Set conversation name on label
-        self.message_label.SetLabel(f"{self.main_window.i18n.t('type_message')} {self.conversation_name}")
+        if not self.conversation.get("key", {}).get("isGroup", False):
+            self.message_label.SetLabel(f"{self.main_window.i18n.t('type_message')} {self.conversation_name}")
+        else:
+            self.message_label.SetLabel(f"{self.main_window.i18n.t('type_message_group')} {self.conversation_name}")
         self.conversation_panel.Show()
         self.message_field.SetFocus()
+
+    def create_accelerator_table(self):
+        #Set IDs
+        self.ID_CTRL_R = wx.NewIdRef()
+        self.ID_ESC = wx.NewIdRef()
+        #create accelerator table
+        accel_tbl = wx.AcceleratorTable([
+            (wx.ACCEL_CTRL, ord('R'), self.ID_CTRL_R),
+            (wx.ACCEL_NORMAL, wx.WXK_ESCAPE, self.ID_ESC)
+        ])
+        self.SetAcceleratorTable(accel_tbl)
+        self.Bind(wx.EVT_MENU, self.on_record_voice_message, id=self.ID_CTRL_R)
+        self.Bind(wx.EVT_MENU, self.close_conversation, id=self.ID_ESC)
+    def on_record_voice_message(self, event):
+        pass
+
+    def close_conversation(self, event):
+        self.conversation_panel.Hide()
+        self.conversations_list.SetFocus()
